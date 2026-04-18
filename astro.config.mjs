182b77c7d1c3eeccf81d2a3ae87665d1c06fbc8c
schadokar <shubhamchadokar04@gmail.com>
2026-04-18 17:37:21 +0530

    feat: import zen mode CSS in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,29 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import mermaid from 'astro-mermaid';
+import expressiveCode from 'astro-expressive-code';
+import { readingTimeRemarkPlugin } from './src/lib/readingTime.ts';
+import starlightViewModes from 'starlight-view-modes'
+import './src/styles/zen-mode.css';
 
 // https://astro.build/config
 export default defineConfig({
+	markdown: {
+		remarkPlugins: [readingTimeRemarkPlugin],
+	},
 	integrations: [
+		expressiveCode({
+			themes: ['github-light', 'github-dark'],
+			styleOverrides: {
+				borderRadius: '0.5rem',
+				codeFontSize: '0.875rem',
+			},
+		}),
+		mermaid(), // ⚠️ Must come BEFORE starlight
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Schadokar Notes',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/schadokar/schadokar-notes' }],
 			sidebar: [
 				{
 					label: 'Design Patterns',
@@ -22,6 +38,8 @@ export default defineConfig({
 					autogenerate: { directory: 'notes' },
 				},
 			],
+			lastUpdated: true,
+			plugins: [starlightViewModes()],
 		}),
 	],
 });
